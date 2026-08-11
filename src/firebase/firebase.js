b52a667795abfe87ab1075042796922d1df0c48f
Rahul N P <rnprahul20@gmail.com>
2026-08-11 11:30:05 +0530

    refactor: use environment variables for Firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,22 +3,19 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD1G4o3gTqpouCgRnW2KF0JZtmg_k_p5mI",
-  authDomain: "kitchenconnect-eddfd.firebaseapp.com",
-  projectId: "kitchenconnect-eddfd",
-  storageBucket: "kitchenconnect-eddfd.firebasestorage.app",
-  messagingSenderId: "366045967849",
-  appId: "1:366045967849:web:fba056d79dcbac0e9e0778",
-  measurementId: "G-7LQ1G7BNLJ",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
-// Export Authentication
 export const auth = getAuth(app);
 
-// Export Firestore
 export const db = getFirestore(app);
 
-// Export Firebase App
 export default app;
